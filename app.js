a461457995e11916fef0501f0e30f4d3d8c554c9
David Washington <dwashin@outlook.com>
2015-05-11 21:25:36 -0500

    game orientation

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = express();
 app.use(express.static('public'));
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
 var port = process.env.PORT || 3000;
+
+var usernames = {};
+var numUsers = 0;
 
 app.get('/', function(req, res) {
  res.sendFile(__dirname + '/public/index.html');
@@ -12,16 +14,40 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
     console.log('a user is connected');
+    
+    socket.username =  "name "+Math.random();
+    socket.color = numUsers < 1 ? 'white' : 'black';
+    usernames[socket.username] = socket.username;
+    ++numUsers;
+    
+    socket.emit('join', {
+        color: socket.color
+    })
 
-    socket.on('button', function(msg) {
-        io.emit('button', msg);
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      color: socket.color,
+      numUsers: numUsers
     });
+    
+    socket.on('move', function(msg) {
+        socket.broadcast.emit('move', msg);
+        console.log(msg);
+    });
+
 
     socket.on('disconnect', function() {
         console.log('a user is disconnected');
-    })
-})
+         delete usernames[socket.username];
+        --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    });
+});
 
 http.listen(port, function() {
     console.log('listening on *: ' + port);
-})
+});
--- public/default.js
+++ public/default.js
@@ -1,19 +1,39 @@
-var game, board;
+var game, board, socket, playerColor;
 
 window.init = function () {    
-    game = new Chess();
-    board = new ChessBoard('board', cfg);
-
-    var socket = io();
-    $('#go').click(function () {
-        socket.emit('button', $('#name').val());
-        //socket.emit('button', "dude");
+    socket = io();
+    
+    socket.on('join', function(msg) {
+      console.log("joined as " + msg.color );
+      playerColor = msg.color;
+      initGame();
+    });
+    
+    socket.on('user joined', function (msg) {
+      console.log(msg.name + " joined as " + msg.color );
     });
 
-    socket.on('button', function (msg) {
-        $('#messages').append($('<li>').text(msg));
+    socket.on('move', function (msg) {
+      game.move(msg);
+      board.position(game.fen());
     });
 };
+
+var initGame = function () {
+ 
+    var cfg = {
+      draggable: true,
+      showNotation: false,
+      orientation: playerColor,
+      position: 'start',
+      onDragStart: onDragStart,
+      onDrop: onDrop,
+      onSnapEnd: onSnapEnd
+    };
+
+    game = new Chess();
+    board = new ChessBoard('board', cfg);
+}
 
 
 
@@ -36,7 +56,11 @@ var onDrop = function(source, target) {
   });
 
   // illegal move
-  if (move === null) return 'snapback';
+  if (move === null) { 
+    return 'snapback';
+  } else {
+     socket.emit('move', move);
+  }
 
 };
 
@@ -44,14 +68,6 @@ var onDrop = function(source, target) {
 // for castling, en passant, pawn promotion
 var onSnapEnd = function() {
   board.position(game.fen());
-};
-
-var cfg = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd
 };
 
 
